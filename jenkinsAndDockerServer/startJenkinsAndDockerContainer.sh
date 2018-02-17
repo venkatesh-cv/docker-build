633a60211c3135a7aeafbd02b57c63ccb769a9c2
venkatesh-cv <venkatesh.cv@gmail.com>
2018-02-17 18:45:56 +0530

    fixed missing volume mapping for /var/jenkins_home

--- jenkinsAndDockerServer/startJenkinsAndDockerContainer.sh
+++ jenkinsAndDockerServer/startJenkinsAndDockerContainer.sh
@@ -1,1 +1,1 @@
-docker run -i -p 8080:8080 -p 50000:50000  -v /var/run/docker.sock:/var/run/docker.sock --name jenkins cvenkatesh/jenkins-docker-server 
+docker run -i -p 8080:8080 -p 50000:50000  -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name jenkins cvenkatesh/jenkins-docker-server 
